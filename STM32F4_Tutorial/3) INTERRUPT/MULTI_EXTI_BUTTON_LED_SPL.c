00f5215f3b1bc6c64a432c9832cab24ea228fe2a
Hayati Kılıç <83482374+kingssize19@users.noreply.github.com>
2024-10-20 12:16:23 +0300

    Create MULTI_EXTI_BUTTON_LED_SPL.c

---
+++ STM32F4_Tutorial/3) INTERRUPT/MULTI_EXTI_BUTTON_LED_SPL.c
@@ -0,0 +1,144 @@
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+
+GPIO_InitTypeDef GPIO_InitStruct;
+EXTI_InitTypeDef EXTI_InitStruct;
+NVIC_InitTypeDef NVIC_InitStruct;
+
+void GPIO_Config(void)
+{
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);	//led
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);	//button
+
+
+	//led config
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_7;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	//button config
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_3;
+	GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+
+	GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+
+
+}
+
+void EXTI_Config(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);	//exti
+
+	//exti config
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB, EXTI_PinSource3);
+
+	EXTI_InitStruct.EXTI_Line = EXTI_Line3;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+
+	EXTI_Init(&EXTI_InitStruct);
+
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB, EXTI_PinSource4);
+
+	EXTI_InitStruct.EXTI_Line = EXTI_Line4;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+
+	EXTI_Init(&EXTI_InitStruct);
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI3_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+
+	NVIC_Init(&NVIC_InitStruct);
+
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI4_IRQn;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 1;
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+
+	NVIC_Init(&NVIC_InitStruct);
+
+}
+
+void delay (uint32_t time)
+{
+	while (time--);
+}
+
+void EXTI3_IRQHandler()
+{
+	if (EXTI_GetITStatus(EXTI_Line3) != RESET) {
+
+		GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+		for (int i = 0; i < 3; ++i){
+
+					GPIO_SetBits(GPIOA, GPIO_Pin_7);
+					delay(5000000);
+					GPIO_ResetBits(GPIOA, GPIO_Pin_7);
+					delay(5000000);
+				}
+		EXTI_ClearITPendingBit(EXTI_Line3);
+	}
+}
+
+
+void EXTI4_IRQHandler()
+{
+	if (EXTI_GetITStatus(EXTI_Line4) != RESET) {
+
+		GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+		for (int i = 0; i < 3; ++i){
+
+			GPIO_SetBits(GPIOA, GPIO_Pin_5);
+			delay(16800000);
+			GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+			delay(16800000);
+		}
+		EXTI_ClearITPendingBit(EXTI_Line4);
+	}
+}
+
+
+int main(void)
+{
+	GPIO_Config();
+	EXTI_Config();
+  while (1)
+  {
+	  GPIO_ResetBits(GPIOA, GPIO_Pin_5);
+  }
+}
+
+
+/*
+ * Callback used by stm32f4_discovery_audio_codec.c.
+ * Refer to stm32f4_discovery_audio_codec.h for more info.
+ */
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size){
+  /* TODO, implement your code here */
+  return;
+}
+
+/*
+ * Callback used by stm324xg_eval_audio_codec.c.
+ * Refer to stm324xg_eval_audio_codec.h for more info.
+ */
+uint16_t EVAL_AUDIO_GetSampleCallBack(void){
+  /* TODO, implement your code here */
+  return -1;
+}
